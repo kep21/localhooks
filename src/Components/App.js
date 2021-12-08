e8cd46c0587b8f1be74fc832090b63eb3945fea7
kep21 <kep135210@gmail.com>
2021-12-08 17:01:44 +0200

    2 спроба

---
+++ src/Components/App.js
@@ -0,0 +1,12 @@
+import Form1 from "./Form1";
+
+const App = () => {
+	return(
+		<div className="container">
+			<h1>Local Storage with hooks</h1>
+			<Form1/>
+		</div>
+	)
+}
+
+export default App;
---
+++ src/Components/Form1.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+
+
+const Form1 = () => {
+	const [name, setName] = useState("");
+
+	useEffect(()=> {
+		localStorage.setItem("name", JSON.stringify(name))
+	}, [name])
+	return (
+		<form>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        placeholder="Full name"
+        aria-label="fullname"
+      />
+      <input type="submit" value="Submit"></input>
+    </form>
+	)
+}
+export default Form1;
